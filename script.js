e8595a713c1116816a8dd129623bdf45fdba1146
hexps13 <irischen0505@gmail.com>
2023-05-25 00:17:23 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var map = L.map('map', {
+    center: [40.75, -74.2],
+    zoom: 13
+  });
+  
+  var imageUrl = '/assets/orbis_map.svg',
+    imageBounds = [
+      [40.712216, -74.22655],
+      [40.773941, -74.12544]
+    ];
+  
+  L.imageOverlay(imageUrl, imageBounds).addTo(map);
