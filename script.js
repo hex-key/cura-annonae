6faaa9a923d65b908afa9f280d4bf89c95d96b6f
hex <irischen0505@gmail.com>
2023-06-04 13:39:52 -0700

    stop

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
-function toggleMapPhase(name) {
-    var textBox = document.getElementById(name + "-para");
-    if (textBox.hidden) {
-        textBox.hidden = false;
-    } else {
-        textBox.hidden = true;
+function toggleView() {
+    var mapView = document.getElementById("map-view");
+    var readerView = document.getElementById("reader-view");
+    var button = document.querySelector(`[id^="view-toggle"]`);
+
+    if (button.id == "view-toggle to-reader") {
+        mapView.style.display = "none";
+        readerView.style.display = "block";
+
+        button.id = "view-toggle to-map";
+        button.innerHTML = "View Map";
+
+    } else if (button.id == "view-toggle to-map") {
+        mapView.style.display = "block";
+        readerView.style.display = "none";
+
+        button.id = "view-toggle to-reader";
+        button.innerHTML = "View Reading Pane";
     }
 }
 
-function toggleReaderView() {
+function toggleMapPhase(name) {
+    // change subtitle and displayed image 
+}
+
+function toggleReaderMode() {
     var window = document.getElementById("reader-window");
     var button = document.querySelector(`[id^="reader-toggle"]`);
-    console.log(button.id);
     if (button.id == "reader-toggle to-essay") {
         window.src = "./assets/essay.html"
     
         button.id = "reader-toggle to-translation";
-        button.onclick = function() { toggleReaderView("translation"); };
+        button.onclick = function() { toggleReaderMode("translation"); };
         button.innerHTML = "View Translation";
 
     } else if (button.id == "reader-toggle to-translation") {
         window.src = "./assets/translation.html"
 
         button.id = "reader-toggle to-essay";
-        button.onclick = function() { toggleReaderView("essay"); };
+        button.onclick = function() { toggleReaderMode("essay"); };
         button.innerHTML = "View Essay";
     }
 }
